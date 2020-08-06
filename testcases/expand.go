0006dac10be3e36a8392953f20029dd698fd914d
Julius Volz <julius.volz@gmail.com>
2020-08-06 16:42:06 +0200

    Add InfluxData copyright notice to expand.go
    Signed-off-by: Julius Volz <julius.volz@gmail.com>

--- testcases/expand.go
+++ testcases/expand.go
@@ -1,3 +1,34 @@
+// Some of this code has been taken and adapted from InfluxData:
+// https://github.com/influxdata/influxdb/blob/26fdb792ffd74f773c253df5d9bebf64ef2b3214/query/promql/internal/promqltests/tests.go
+//
+// The original copyright notice and license of that code is reproduced here:
+//
+// -------------------------------------------------------------------------------
+//
+// MIT License
+
+// Copyright (c) 2018 InfluxData
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+// -------------------------------------------------------------------------------
+
 package testcases
 
 import (
